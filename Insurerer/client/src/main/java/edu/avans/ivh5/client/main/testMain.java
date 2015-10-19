4b5773c16d33f2b24d3c4b228c84f49319af1f28
Rico Bakels <Ricobakels2@hotmail.com>
2015-10-19 13:10:28 +0200

    LoginGUi is verder gemaakt

---
+++ Insurerer/client/src/main/java/edu/avans/ivh5/client/main/testMain.java
@@ -0,0 +1,25 @@
+package edu.avans.ivh5.client.main;
+
+import edu.avans.ivh5.client.businesslogic.LoginManager;
+import edu.avans.ivh5.client.presentation.LoginGUI;
+import edu.avans.ivh5.client.presentation.MainGUI;
+
+public class testMain {
+    
+    /*private LoginGUI loginGUI;
+    private LoginManager loginManager;
+    private MainGUI mainManager;*/
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginManager loginManager = new LoginManager();
+        MainGUI mainGUI = new MainGUI();
+        
+        LoginGUI loginGUI = new LoginGUI(loginManager, mainGUI);
+        
+        
+    }
+    
+}
--- Insurerer/client/src/main/java/edu/avans/ivh5/client/presentation/LoginGUI.java
+++ Insurerer/client/src/main/java/edu/avans/ivh5/client/presentation/LoginGUI.java
@@ -16,13 +16,17 @@ public class LoginGUI extends javax.swing.JFrame {
     /**
      * Creates new form LoginGUII
      */
-    private LoginManager loginManager;
-    private MainGUI mainManager;
+    private final LoginManager loginManager;
+    private final MainGUI mainGUI;
 
-    public LoginGUI(LoginManager loginManager, MainGUI mainManager) {
+    public LoginGUI(LoginManager loginManager, MainGUI mainGUI) {
         this.loginManager = loginManager;
-        this.mainManager = mainManager;
+        this.mainGUI = mainGUI;
+        this.setVisible(true);
         initComponents();
+        errorLabel.setVisible(false);
+
+        loginButtonActionPerformed();
     }
 
     /**
@@ -39,6 +43,9 @@ public class LoginGUI extends javax.swing.JFrame {
         wachtwoordLabel = new javax.swing.JLabel();
         gebruikersnaamField = new javax.swing.JTextField();
         wachtwoordField = new javax.swing.JTextField();
+        loginButton = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        errorLabel = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -49,35 +56,55 @@ public class LoginGUI extends javax.swing.JFrame {
 
         wachtwoordLabel.setText("Wachtwoord");
 
-        gebruikersnaamField.setText("gebruikersnaam");
         gebruikersnaamField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 gebruikersnaamFieldActionPerformed(evt);
             }
         });
 
-        wachtwoordField.setText("wachtwoord");
         wachtwoordField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 wachtwoordFieldActionPerformed(evt);
             }
         });
 
+        loginButton.setText("Inloggen");
+
+        errorLabel.setForeground(new java.awt.Color(255, 0, 0));
+        errorLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        errorLabel.setText("jLabel2");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(215, 215, 215)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(140, 140, 140)
+                        .addComponent(loginButton, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(48, 48, 48)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(loginLabel)
-                    .addComponent(gebruikersnaamLabel)
-                    .addComponent(wachtwoordLabel))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 58, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(gebruikersnaamField, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(wachtwoordField, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(109, 109, 109))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(loginLabel)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(wachtwoordLabel)
+                                    .addComponent(gebruikersnaamLabel))
+                                .addGap(58, 58, 58)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(gebruikersnaamField, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(wachtwoordField, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addGap(76, 76, 76))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(errorLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 243, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(68, 68, 68))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -92,11 +119,35 @@ public class LoginGUI extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(wachtwoordLabel)
                     .addComponent(wachtwoordField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(123, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(loginButton, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(58, 58, 58)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(errorLabel)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void loginButtonActionPerformed() {
+        // TODO add your handling code here:
+        loginButton.addActionListener(e -> {
+            errorLabel.setVisible(false);
+
+            if (gebruikersnaamField.getText().length() == 0 || wachtwoordField.getText().length() == 0) {
+                errorLabel.setText("naam en/of wachtwoord is leeg");
+                errorLabel.setVisible(true);
+            } else if(!loginManager.isLoginValid(gebruikersnaamField.getText(), wachtwoordField.getText())){
+                errorLabel.setText("inloggegevens zijn onjuist");
+                errorLabel.setVisible(true);
+            }
+            else{
+                loginManager.login(gebruikersnaamField.getText(), wachtwoordField.getText());
+            }
+        });
+    }
 
     private void gebruikersnaamFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_gebruikersnaamFieldActionPerformed
         // TODO add your handling code here:
@@ -111,8 +162,11 @@ public class LoginGUI extends javax.swing.JFrame {
      */
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel errorLabel;
     private javax.swing.JTextField gebruikersnaamField;
     private javax.swing.JLabel gebruikersnaamLabel;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton loginButton;
     private javax.swing.JLabel loginLabel;
     private javax.swing.JTextField wachtwoordField;
     private javax.swing.JLabel wachtwoordLabel;
