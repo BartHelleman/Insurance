0012bc536361f493ea85771913075f4eda74519a
Bart Helleman <bart_helleman@hotmail.nl>
2015-10-19 15:28:40 +0200

    Insurance2

--- Insurerer/client/src/main/java/edu/avans/ivh5/client/main/Main.java
+++ Insurerer/client/src/main/java/edu/avans/ivh5/client/main/Main.java
@@ -3,8 +3,13 @@ package edu.avans.ivh5.client.main;
 import edu.avans.ivh5.client.presentation.ClientGUI;
 import javax.swing.UIManager;
 
+import edu.avans.ivh5.client.businesslogic.InsuranceManager;
+import edu.avans.ivh5.client.presentation.InsuranceGUI;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("aaa");
+        InsuranceManager insuranceManager = new InsuranceManager();
+        InsuranceGUI insuranceGUI = new InsuranceGUI(insuranceManager);
+        insuranceGUI.setVisible(true);
     }
 }
