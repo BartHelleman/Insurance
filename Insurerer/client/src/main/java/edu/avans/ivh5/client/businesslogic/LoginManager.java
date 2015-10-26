264b22954fbe6a44c62a6b88aff6c89d7a0998a5
Rico Bakels <Ricobakels2@hotmail.com>
2015-10-26 10:11:47 +0100

    iets

--- Insurerer/client/src/main/java/edu/avans/ivh5/client/businesslogic/LoginManager.java
+++ Insurerer/client/src/main/java/edu/avans/ivh5/client/businesslogic/LoginManager.java
@@ -2,16 +2,25 @@ package edu.avans.ivh5.client.businesslogic;
 
 import edu.avans.ivh5.server.dao.LoginDAO;
 import edu.avans.ivh5.shared.models.User;
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
 
 public class LoginManager {
-    
-    private LoginDAO loginDAO = new LoginDAO();
+
     private User user;
-    
+    private LoginDAO loginDAO;
+
     public boolean login(String username, String password) {
-        
-        user = (User)loginDAO.get(username).get(0);
-        
+
+        try {
+            loginDAO = new LoginDAO();
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            System.out.println("Er is een exception");
+        }
+
+        user = (User) loginDAO.get(username).get(0);
+
         if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
             // Login succesful
             return true;
@@ -20,8 +29,8 @@ public class LoginManager {
             return false;
         }
     }
-    
+
     public void logout() {
-        
+
     }
 }
