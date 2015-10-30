e77ba7f03c8e147cd18eaf3e1c35c5c94b31c297
Burak <bkaratas1@avans.nl>
2015-10-30 13:24:15 +0100

    CRUD operaties toegevoegd aan InsuranceCompanyDAO

--- Insurerer/server/src/main/java/edu/avans/ivh5/server/dao/InsuranceCompanyDAO.java
+++ Insurerer/server/src/main/java/edu/avans/ivh5/server/dao/InsuranceCompanyDAO.java
@@ -4,7 +4,9 @@ import edu.avans.ivh5.shared.models.InsuranceCompany;
 import java.io.IOException;
 import java.util.*;
 import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.Text;
 import org.xml.sax.SAXException;
 
 public class InsuranceCompanyDAO implements DAOInterface {
@@ -17,7 +19,44 @@ public class InsuranceCompanyDAO implements DAOInterface {
 
     @Override
     public boolean add(Object item) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(item instanceof InsuranceCompany)
+        {
+            InsuranceCompany company = (InsuranceCompany)item;
+            Element companyNode = this.XMLParser.createElement("company");
+            
+            Element name = this.XMLParser.createElement("name");
+            Text nameText = this.XMLParser.createTextNode(company.getName());
+            name.appendChild(nameText);
+            companyNode.appendChild(name);
+            
+            Element city = this.XMLParser.createElement("city");
+            Text cityText = this.XMLParser.createTextNode(company.getCity());
+            city.appendChild(cityText);
+            companyNode.appendChild(city);
+            
+            Element postcode = this.XMLParser.createElement("postcode");
+            Text postcodeText = this.XMLParser.createTextNode(company.getPostCode());
+            postcode.appendChild(postcodeText);
+            companyNode.appendChild(postcode);
+            
+            Element address = this.XMLParser.createElement("address");
+            Text addressText = this.XMLParser.createTextNode(company.getAddress());
+            address.appendChild(addressText);
+            companyNode.appendChild(address);
+            
+            Element KVK = this.XMLParser.createElement("KVK");
+            Text KVKText = this.XMLParser.createTextNode(company.getKVK());
+            KVK.appendChild(KVKText);
+            companyNode.appendChild(KVK);
+            
+            this.XMLParser.addNode(companyNode);
+            
+            DAOInterface.save(this.XMLParser.getXmlFile(), this.XMLParser.getDocument());
+            return true;
+        }
+        
+        return false;
     }
 
     @Override
@@ -31,12 +70,34 @@ public class InsuranceCompanyDAO implements DAOInterface {
 
     @Override
     public boolean change(Object oldObject, Object newObject) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(!(oldObject instanceof InsuranceCompany) || !(newObject instanceof InsuranceCompany))
+            throw new RuntimeException("Old and new insurancecompanies have to be of type: InsuranceCompany");
+        
+        InsuranceCompany oldCompany = (InsuranceCompany)oldObject;
+        InsuranceCompany newCompany = (InsuranceCompany)newObject;
+        
+        if(delete(oldCompany))
+            return add(newCompany);
+        return false;
     }
 
     @Override
     public boolean delete(Object value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<Object> result = get(value.toString());
+        if(result.size() != 1)
+            return false;
+        
+        if(value instanceof String)
+        {
+            InsuranceCompany companyToDelete = (InsuranceCompany)result.get(0);
+            List<Node> nodes = this.XMLParser.findElementsByName("company", companyToDelete.getKVK());
+            for(Node node : nodes) {
+                this.XMLParser.deleteNode(node);
+            }
+            DAOInterface.save(this.XMLParser.getXmlFile(), this.XMLParser.getDocument());
+            return true;
+        }
+        return false;
     }
 
     private List<Object> getCompanies(String searchPattern) {
